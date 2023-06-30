3fd2696a4d04c59e2388954e0f5fe0d379132410
Gabrito22 <g.izquierdo.2018@alumnos.urjc.es>
2023-06-30 08:00:30 +0200

    productcontroller funcionalidad guardar producto, logger

--- src/main/java/com/dwsiapp/controller/ProductController.java
+++ src/main/java/com/dwsiapp/controller/ProductController.java
@@ -1,12 +1,25 @@
 package com.dwsiapp.controller;
 
+
+import com.dwsiapp.models.Product;
+import com.dwsiapp.models.User;
+import com.dwsiapp.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    private ProductService productService; //
     @GetMapping("")
     public String show(){
         return "products/show";
@@ -16,4 +29,13 @@ public class ProductController {
     public String create(){
         return "products/create";
     }
+    @PostMapping("/save")
+    public String save(Product product){
+        LOGGER.info("Este es el producto de la vista {}",product);
+        User u = new User(1,"","","","","","","");
+        product.setUser(u);
+        productService.save(product);
+        return "redirect:/products";
+    }
+
 }

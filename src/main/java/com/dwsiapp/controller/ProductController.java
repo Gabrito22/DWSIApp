3cf14851c269513d014e37f70dcac75c3f73ed09
Gabrito22 <g.izquierdo.2018@alumnos.urjc.es>
2023-07-02 11:43:53 +0200

    producsts

--- src/main/java/com/dwsiapp/controller/ProductController.java
+++ src/main/java/com/dwsiapp/controller/ProductController.java
@@ -1,14 +1,29 @@
 package com.dwsiapp.controller;
 
+import com.dwsiapp.models.Product;
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     @GetMapping("")
     public String show(){
         return "products/show";
+    }
+    @GetMapping("/create")
+    public String create(){
+        return "products/create";
+    }
+    @PostMapping("/save")
+    public String save(Product product){
+        LOGGER.info("This is the object product {}",product);
+        return "redirect:/products";
     }
 }

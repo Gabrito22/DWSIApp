453fee925225f94a3f4a50b6a10f9283ba0cdccd
Gabrito22 <g.izquierdo.2018@alumnos.urjc.es>
2023-07-02 12:34:36 +0200

    producst controller

--- src/main/java/com/dwsiapp/service/ProductService.java
+++ src/main/java/com/dwsiapp/service/ProductService.java
@@ -2,6 +2,7 @@ package com.dwsiapp.service;
 
 import com.dwsiapp.models.Product;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
@@ -9,4 +10,6 @@ public interface ProductService {
     public Optional<Product> get(Integer id); //optional nos permite ver si ese producto se encuentra en la BBDD
     public void update(Product product);
     public void delete(Integer id);
+
+    public List<Product> findAll();
 }
